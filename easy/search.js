8d4808ac4883a6b1e62059d2e033e948dcfae8a0
Eve Cho <e.young.echo@gmail.com>
2023-03-12 01:31:08 -0500

    easy: search.js updated with new solution

--- easy/search.js
+++ easy/search.js
@@ -15,3 +15,11 @@ var search = function(nums, target) {
 
 // time complexity: O(n)
 
+var searchWithLoop = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) return i;
+    }
+    return -1;
+};
+
+// time complexity: O(n)
