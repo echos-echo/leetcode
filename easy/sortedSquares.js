79350275df080b5619b123fab5ea99bd3b14ab97
Eve Cho <e.young.echo@gmail.com>
2023-03-13 00:19:15 -0400

    easy: added sortedSquares.js

---
+++ easy/sortedSquares.js
@@ -0,0 +1,16 @@
+/**
+ * given an array of sorted numbers, return an array in which each value is squared
+ * and then sorted
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    return nums.map(num => num ** 2).sort((x, y) => x - y);
+};
+
+const test1 = [-4,-1,0,3,10];
+const test2 = [-7,-3,2,3,11];
+
+console.log(sortedSquares(test1));  // -> [0,1,9,16,100]
+console.log(sortedSquares(test2));  // -> [4,9,9,49,121]
