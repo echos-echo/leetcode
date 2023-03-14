518cc87eef32ee900129041a9fe5185c9b6d511b
Eve Cho <e.young.echo@gmail.com>
2023-03-14 17:11:25 -0400

    easy: added reverseWords.js

---
+++ easy/reverseWords.js
@@ -0,0 +1,12 @@
+/**
+ * string s contains words; reverse the characters in each word while preserving word order
+ * 
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let sAsArray = s.split(' ');
+    let wordsReversed = sAsArray.map(word => word.split('').reverse().join(''));
+    return wordsReversed.join(' ');
+};
+
